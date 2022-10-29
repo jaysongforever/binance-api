67907e9e057879ebd22b7d079349f762e6f4ad56
‘JaySong’ <526678604@qq.com>
2022-10-29 15:15:45 +0800

    提交内容过滤

---
+++ request.js
@@ -0,0 +1,14 @@
+const { Spot } = require('@binance/connector')
+const HttpsProxyAgent = require('https-proxy-agent')
+const httpsAgent = new HttpsProxyAgent('http://127.0.0.1:7890')
+
+const { apiKey, apiSecret } = require('./config')
+
+const client = new Spot(apiKey, apiSecret, {
+  httpsAgent,
+  proxy: false
+})
+
+// Get account information
+// client.account().then(response => client.logger.log(response.data))
+module.exports = client
